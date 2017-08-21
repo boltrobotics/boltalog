3eaa1faa488d0fb695a4d443fd47ce6fd383603f
Sergey Kapustin <svkapustin@gmail.com>
2017-08-21 14:38:49 -0400

    Add unit tests. Adjust model files - Added/modified unit tests - Moved logger constructor to private section - Added level setter - Added check for level in filter getter - Moved static logger to class scope

--- src/test/logger_test.cpp
+++ src/test/logger_test.cpp
@@ -5,28 +5,82 @@
 // SYSTEM INCLUDES
 #include <gtest/gtest.h>
 #include <iostream>
+#include <fstream>
 
 // PROJECT INCLUDES
 #include "logger.hpp"
 
 namespace slog {
 
-TEST(TestCase, TestName)
-{
-    Logger::make("logger");
+std::vector<std::string> readLastLines(
+        uint8_t& start_idx,
+        uint8_t count,
+        const std::string& fname = "/var/log/user.log") {
+
+    start_idx = 0;
+    std::ifstream f(fname);
+    std::vector<std::string> lines(count, "");
+
+    if (f.is_open()) {
+        std::string line;
+
+        while (std::getline(f, line)) {
+            lines[start_idx % count] = line;
+            ++start_idx;
+        }
+    }
+    return lines;
+}
+
+TEST(LoggerTest, instance) {
+    Logger* logger = Logger::make("logger_test");
+    EXPECT_EQ(logger, Logger::instance());
+}
+
+TEST(LoggerTest, Level) {
+    Logger* logger = Logger::make("logger_test");
+
+    for (int i = 0; i < 4; i++) {
+        EXPECT_EQ(logger->filter(i), true);
+    }
+
+    logger->level(0, LOG_WARNING);
+    EXPECT_EQ(logger->filter(0), true);
+    EXPECT_EQ(logger->filter(1), false);
+    EXPECT_EQ(logger->filter(2), true);
+    EXPECT_EQ(logger->filter(3), false);
+
+    logger->level(1, LOG_ERR);
+    logger->level(3, LOG_WARNING);
+
+    for (int i = 0; i < 4; i++) {
+        EXPECT_EQ(logger->filter(i), true);
+    }
+
+    EXPECT_EQ(-1, logger->level(4, LOG_INFO));
+}
+
+TEST(LoggerTest, Output) {
+    // Make sure rsyslog is configured to output LOG_USER logs to /var/log/user.log
+    // and the file is readable.
+    Logger::make("logger_test");
     onLogEventA(32, 8);
     onLogEventB(16, "param2");
     onLogEventC(64, "p2", "p3", 4);
-}
 
-TEST(DISABLED_TestCase, TestName2)
-{
-    EXPECT_EQ(1, 0);
-}
+    std::vector<std::string> expected;
+    expected.push_back("1,32,8");
+    expected.push_back("2,16,param2");
+    expected.push_back("3,64,p2,p3,4");
 
-TEST(TestCase, TestName3)
-{
-    EXPECT_EQ(1, 1);
+    uint8_t start_idx = 0;
+    uint8_t count = 3;
+    auto lines = readLastLines(start_idx, count);
+
+    for (uint8_t i = 0; i < count; i++, start_idx++) {
+        std::size_t found = lines[start_idx % count].find(expected[i]);
+        EXPECT_NE(std::string::npos, found) << "Not found: " << expected[i];
+    }
 }
 
 } // namespace slog

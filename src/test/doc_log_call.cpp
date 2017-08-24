a4a30da97a0280b19acd7ffda180daead334ce34
Sergey Kapustin <svkapustin@gmail.com>
2017-08-24 13:44:46 -0400

    Add source code to support github post - Page: https://svkapustin.github.io

---
+++ src/test/doc_log_call.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include <syslog.h>
+#include <iostream>
+	
+#define onLogEvent(slow_stuff) \
+    if (LOG_INFO <= log_level_) { \
+        log(slow_stuff); \
+    }
+
+int log_level_ = LOG_WARNING;
+
+void log(const std::string&) {
+	if (LOG_INFO <= log_level_) {
+        std::cout << "Event is output" << std::endl;
+	}
+}
+
+std::string getStuffSlowly() {
+	std::string blah = "hmmm... blah blah";
+	
+	for (int i = 0; i < 27; i++) {
+		blah += blah;
+	}
+	return blah;
+}
+
+TEST(Doc, BadLogCall) {
+    log(getStuffSlowly());
+}
+
+TEST(Doc, GoodLogCall) {
+	if (LOG_INFO <= log_level_) {
+        log(getStuffSlowly());
+    }
+}
+
+TEST(Doc, GoodLogCallMacro) {
+    onLogEvent(getStuffSlowly());
+}
